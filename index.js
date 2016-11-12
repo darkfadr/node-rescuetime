d325c829901466b9fba3b596ab9ec6079024ae20
Ashley Narcisse-Austin <Ashley_NarcisseAustin@ultimatesoftware.com>
2016-11-11 23:23:31 -0500

    cleaned code abit and added rates and invoices

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
 // import GolfNow from 'lib/api';
-var GolfNow = require('./lib/api');
-
+var GolfNow = require('./lib/golfnow');
 
 const config = {
 	client_id: 'EBYP2DTX3MFHN4QZNTB4EHVYMF0YIRHL',
 	client_secret: '4E2S-W_DPL^Ev&;L6xsi',
-	channel: '19818'
+	channel: 19818
 };
 
 const api = new GolfNow(config);
 
-api.courses({
+/*api.courses({
 		latitude: 25.7746303,
 		longitude: -80.1930857,
 		proximity: 50
 	})
 	.then(res => console.log(res.data))
-	.catch(err => console.log(err));
-/*
-api.courses()
+	.catch(err => console.log(err)); */
+
+api.rateTags('CI')
 	.then(res => console.log(res.data))
-	.catch(err => console.log(err));*/
+	.catch(err => console.log(err));
