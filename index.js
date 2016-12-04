246e5e5edca05546981105d9affd92849fd24f66
Ashley Narcisse-Austin <Ashley_NarcisseAustin@ultimatesoftware.com>
2016-12-04 18:24:39 -0500

    cleaned a tweaked a few things

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-// import GolfNow from 'lib/api';
 const rt = require('./lib/rescuetime');
-const key = 'B63A6Z9UdENcLLYpA_qRpETJgZxO_s8vOpF1bOXp';
-
-const api =  rt(key);
+const api =  rt('API_KEY_HERE');
 
 api.analytics({
 	perspective: 'interval',
 	restrict_kind: 'productivity'
 })
-	.then(d => console.log(d))
-	.catch(err => console.log('fail'));
+.then(d => console.log(d.data))
+.catch(err => console.log('fail'));
